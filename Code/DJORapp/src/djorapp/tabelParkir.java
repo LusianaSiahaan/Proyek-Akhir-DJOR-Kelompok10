089126eb803324a9d25827a1b8e2da88ad739791
Lusiana R. R. Siahaan <60680140+LusianaSiahaan@users.noreply.github.com>
2020-06-15 15:06:56 +0700

    FIXED tableParkir

--- Code/DJORapp/src/djorapp/tabelParkir.java
+++ Code/DJORapp/src/djorapp/tabelParkir.java
@@ -89,8 +89,8 @@ ResultSet rs = statement.executeQuery();
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 199, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
